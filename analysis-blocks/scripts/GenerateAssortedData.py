d12e4aac54238d7fdbbbccf3d2438632af5dc30f
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-11-29 21:35:37 +0000

    OPENMETA-335 Added more types to GenerateAssortedData.py.

--- analysis-blocks/scripts/GenerateAssortedData.py
+++ analysis-blocks/scripts/GenerateAssortedData.py
@@ -11,30 +11,42 @@ class GenerateAssortedData(Component):
 
         self.add_param("m", val=1.0)
         self.add_param("n", val=1.0)
-        self.add_output("scalar_out", val=0.0)
-        self.add_output("scalararray_out", val=[0.0, 0.0], pass_by_obj=True)
-        self.add_output("npscalararray_out", val=np.array([[0.0, 0.0], [0.0, 0.0]]), pass_by_obj=True)
-        self.add_output("string_out", val=u"None", pass_by_obj=True)
-        self.add_output("stringarray_out", val=[u"", u""], pass_by_obj=True)
-        self.add_output("npstringarray_out", val=np.array([u"", u""]), pass_by_obj=True)
+        self.add_output("float_out", val=0.0)
+        self.add_output("floatarray_out", val=[0.0, 0.0], pass_by_obj=True)
+        self.add_output("npfloatarray_out", val=np.array([[0.0, 0.0], [0.0, 0.0]]), pass_by_obj=True)
+        self.add_output("int_out", val=0)
+        self.add_output("intarray_out", val=[0, 0], pass_by_obj=True)
+        self.add_output("npintarray_out", val=np.array([[0, 0], [0, 0]]), pass_by_obj=True)
+        self.add_output("string_out", val="String", pass_by_obj=True)
+        self.add_output("stringarray_out", val=["String 1", "String 2"], pass_by_obj=True)
+        self.add_output("npstringarray_out", val=np.array(["String 1", "String 2"]), pass_by_obj=True)
+        self.add_output("unicode_out", val=u"Unicode", pass_by_obj=True)
+        self.add_output("unicodearray_out", val=[u"Unicode 1", u"Unicode 2"], pass_by_obj=True)
+        self.add_output("npunicodearray_out", val=np.array([u"Unicode 1", u"Unicode 2"]), pass_by_obj=True)
         self.add_output("bool_out", val=True, pass_by_obj=True)
-        self.add_output("dict_homo_out", val={'apples': 3, 'oranges': 2, 'bananas': 1}, pass_by_obj=True)
+        self.add_output("dict_strkey_homo_out", val={'apples': 3, 'oranges': 2, 'bananas': 1}, pass_by_obj=True)
+        self.add_output("dict_unikey_homo_out", val={u'apples': 3, u'oranges': 2, u'bananas': 1}, pass_by_obj=True)
         self.add_output("dict_hetero_out", val={'name': 'alice', 'age': 50, 'children': ['bob', 'caleb']}, pass_by_obj=True)
 
     def solve_nonlinear(self, params, unknowns, resids):
         m = max(int(params["m"]), 1)
         n = max(int(params["n"]), 1)
-        unknowns["scalar_out"] = m * n
-        unknowns["scalararray_out"] = [[y for y in xrange(x * n, x * n + n)] for x in xrange(m)]
-        unknowns["npscalararray_out"] = np.array(xrange(m * n)).reshape(m, n) 
+        unknowns["float_out"] = float(m * n)
+        unknowns["floatarray_out"] = [[float(y) for y in xrange(x * n, x * n + n)] for x in xrange(m)]
+        unknowns["npfloatarray_out"] = np.array(xrange(m * n), dtype=np.float).reshape(m, n)
         unknowns["string_out"] = "m = " + str(m) + "; n = " + str(n)
         unknowns["stringarray_out"] = [str(m), str(n)]
         unknowns["npstringarray_out"] = np.array([str(m), str(n)])
+        unknowns["unicode_out"] = "m = " + unicode(m) + "; n = " + unicode(n)
+        unknowns["unicodearray_out"] = [unicode(m), unicode(n)]
+        unknowns["npunicodearray_out"] = np.array([unicode(m), unicode(n)])
         unknowns["bool_out"] = m > n
-        unknowns["dict_homo_out"]["apples"], unknowns["dict_homo_out"]["oranges"] = m, n
+        unknowns["dict_strkey_homo_out"]["apples"], unknowns["dict_strkey_homo_out"]["oranges"] = m, n
+        unknowns["dict_unikey_homo_out"]["apples"], unknowns["dict_unikey_homo_out"]["oranges"] = m, n
         unknowns["dict_hetero_out"]["children"] = ['bob', 'caleb']
         for name in ["other_child_{0}".format(num+1) for num in xrange(min(m+n,10))]:
             unknowns["dict_hetero_out"]["children"].append(name)
+        
 
 def main():
     top = Problem()
@@ -50,14 +62,21 @@ def main():
     top.setup()
     top.run()
     
-    print('scalar_out =', root.Generator.unknowns['scalar_out'])
-    print('scalararray_out =', root.Generator.unknowns['scalararray_out'])
-    print('npscalararray_out =', root.Generator.unknowns['npscalararray_out'])
+    print('float_out =', root.Generator.unknowns['float_out'])
+    print('floatarray_out =', root.Generator.unknowns['floatarray_out'])
+    print('npfloatarray_out =', root.Generator.unknowns['npfloatarray_out'])
+    print('int_out =', root.Generator.unknowns['int_out'])
+    print('intarray_out =', root.Generator.unknowns['intarray_out'])
+    print('npintarray_out =', root.Generator.unknowns['npintarray_out'])
     print('string_out =', root.Generator.unknowns['string_out'])
     print('stringarray_out =', root.Generator.unknowns['stringarray_out'])
     print('npstringarray_out =', root.Generator.unknowns['npstringarray_out'])
+    print('unicode_out =', root.Generator.unknowns['unicode_out'])
+    print('unicodearray_out =', root.Generator.unknowns['unicodearray_out'])
+    print('npunicodearray_out =', root.Generator.unknowns['npunicodearray_out'])
     print('bool_out =', root.Generator.unknowns['bool_out'])
-    print('dict_homo_out =', root.Generator.unknowns['dict_homo_out'])
+    print('dict_strkey_homo_out =', root.Generator.unknowns['dict_strkey_homo_out'])
+    print('dict_unikey_homo_out =', root.Generator.unknowns['dict_unikey_homo_out'])
     print('dict_hetero_out =', root.Generator.unknowns['dict_hetero_out'])
 
 if __name__ == "__main__":
