9b771ecd208db798e672575b9f052f320b640bb1
Tim Thomas <tthomas@metamorphsoftware.com>
2017-12-01 01:02:41 -0600

    OPENMETA-333 Added example Jenkins CI scripts.

---
+++ continuous-integration/collect_tests_all.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\META\bin\Python27\Scripts\python.exe" "C:\Program Files (x86)\META\bin\RunTestBenches.py" continuous-integration.xme -- -s --with-xunit -v --collect-only
+exit /b 0
