7a88cbbe61238ac7457206f51a0932a26d1404d6
Tim Thomas <tthomas@metamorphsoftware.com>
2017-12-04 21:58:58 +0000

    OPENMETA-335 Added specific Matlab example doubler.

---
+++ analysis-blocks/scripts/DoubleExample.m
@@ -0,0 +1,23 @@
+% variable: dict_str_int_in object input
+% variable: float_in double input
+% variable: npfloatarray_in double[] input
+% variable: npintarray_in double[] input
+% variable: stringarray_in string[] input
+% variable: unicode_in string input
+% variable: unicodearray_in string[] input
+% variable: dict_str_int_out object output
+% variable: float_out double output
+% variable: npfloatarray_out double[] output
+% variable: npintarray_out double[] output
+% variable: stringarray_out string[] output
+% variable: unicode_out string output
+% variable: unicodearray_out string[] output
+
+dict_str_int_out = structfun(@(x) (x * 2), dict_str_int_in, 'uni', 0)
+float_out = float_in * 2
+npfloatarray_out = npfloatarray_in * 2
+npintarray_out = npintarray_in * 2
+stringarray_out = [stringarray_in, stringarray_in]
+unicode_out = strcat(unicode_in, unicode_in)
+unicodearray_out = [unicodearray_in, unicodearray_in]
+
